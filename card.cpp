60a4675b584587fd6d0177acb9010f687493c333
yasunori <0528.taiki.sax@gmail.com>
2024-05-18 21:48:14 +0900

    add files

--- card.cpp
+++ card.cpp
@@ -1,3 +1,38 @@
+const uint64_t rh_mod = 0x1FFFFFFFFFFFFFFF;
+const uint64_t rh_mask30 = 0x3FFFFFFF;
+const uint64_t rh_mask31 = 0x7FFFFFFF;
+const uint64_t rh_mask61 = rh_mod;
+uint64_t rh_base;
+vector<uint64_t> rh_base_pow;
+uint64_t rh_mul(uint64_t a, uint64_t b){
+    uint64_t au = a >> 31;
+    uint64_t ad = a & rh_mask31;
+    uint64_t bu = b >> 31;
+    uint64_t bd = b & rh_mask31;
+    uint64_t mid = ad * bu + au * bd;
+    uint64_t midu = mid >> 30;
+    uint64_t midd = mid & rh_mask30;
+
+    uint64_t x = au * bu * 2 + midu + (midd << 31) + ad * bd;
+    uint64_t xu = x >> 61;
+    uint64_t xd = x & rh_mask61;
+
+    uint64_t rtn = xu + xd;
+    if(rtn >= rh_mod) rtn -= rh_mod;
+    return rtn;
+}
+
+void init_rh_base(int N){
+    rh_base = (uint64_t(1) << 32) + engine();
+    rh_base_pow.resize(N);
+    rh_base_pow[0] = 1;
+    for(int i = 1; i < N; i++){
+        rh_base_pow[i] = rh_mul(rh_base_pow[i-1], rh_base);
+    }
+}
+
+//カードの種類
+//'a'~'n' は 'X' に 0 ~ 13 を代入した状態を指す.
 const string alphabet = "0123456789TJQKXabcdefghijklmn";
 
 struct word {
@@ -5,12 +40,14 @@ struct word {
     string str;
     int digit;
     bigint val;
+    uint64_t hash;
 
     word(){
         len = 0;
         str = "";
         digit = 0;
         val = 0;
+        hash = 0;
     }
 
     word(char c){
@@ -28,6 +65,7 @@ struct word {
         digit = (val < 10) ? 1 : 2;
         len = 1;
         str = string(1, c);
+        hash = rh_base_pow[c];
     }
 
     word operator*(word other){
@@ -36,6 +74,8 @@ struct word {
         rtn.str = str + other.str;
         rtn.digit = digit + other.digit;
         rtn.val = val * pow(bigint(10), other.digit) + other.val;
+        rtn.hash = hash + other.hash;
+        if(rtn.hash >= rh_mod) rtn.hash - rh_mod;
         return rtn;
     }
 };
---
+++ miller_labin.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <gmp.h>
+#include <gmpxx.h>
+using namespace std;
+using bigint = mpz_class;
+
+#include "rand.cpp"
+#include "time.cpp"
+#include "math.cpp"
+#include "card.cpp"
+
+bool miller_rabin(bigint x){
+    if(x <= 1) return false;
+    if(x == 2) return true;
+    if(x % 2 == 0) return false;
+
+    int s = 0;
+    bigint d = x - 1;
+    while(d % 2 == 0){
+        d /= 2;
+        s++;
+    }
+
+    bigint k = 100;//テスト回数
+    if(x - 1 < k) k = x - 1;
+
+    set<bigint> A;
+
+    {
+        bigint a_max = x - 1;
+        while(A.size() < k){
+            A.insert(engine() % a_max + 1);
+        }
+    }
+
+    for(bigint a : A){
+        bigint t = pow_mod(a, d, x);
+        if(t == 1) continue;
+        bool b = false;
+        for(int i = 0; i < s; i++){
+            if(t == x - 1){
+                b = true;
+                break;
+            }
+            t = t * t % x;
+        }
+        if(b) continue;
+        return false;
+    }
+    return true;
+}
