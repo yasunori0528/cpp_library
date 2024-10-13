c7788a3d2bd17c4a570e007e25aa84f921abb037
yasunori0528 <0528.taiki.sax@gmail.com>
2024-10-13 23:23:22 +0900

    a

--- Cpp/01_AtCoder_algorithm/_multiple.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-using bitset<1024> int1024_t;
-
-operator+(int1024_t x, int1024_t y){
-    return (x^y)+((x&y)<<1);
-}
--- atcoder/lib.cpp
+++ atcoder/lib.cpp
@@ -27,3 +27,18 @@ double calcTime()
     return (getTime.tv_sec + getTime.tv_nsec*1e-9) *1000;
 }
 
+//座標圧縮
+template <typename T>
+void compress(vector<T> &A){
+    auto B = A;
+    sort(B.begin(), B.end());
+    B.erase(unique(B.begin(), B.end()), B.end());
+    for(T &i : A){
+        i = lower_bound(B.begin(), B.end(), i) - B.begin();
+    }
+    return;
+}
+
+int main(){
+    
+}
