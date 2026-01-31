329797a38488c7c1807fb34f6abbc7f44728e1f7
yasunori0528 <0528.taiki.sax@gmail.com>
2026-01-31 09:48:37 +0900

    CardString.cppを作成

---
+++ library/PrimeQk/CardString.cpp
@@ -0,0 +1,15 @@
+// CardString.cpp
+// date : 2026/01/31
+// author : yasunori
+
+#ifndef CARD_STRING_CPP
+#define CARD_STRING_CPP
+
+#include "Head.h"
+#include "Card.cpp"
+
+class CardString {
+	
+};
+
+#endif	// CARD_STRING_CPP
